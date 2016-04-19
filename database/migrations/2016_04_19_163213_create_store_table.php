88c45403d37511c7f74a09290e280b2250c66fb2
18759758958 <18759758958@163.com>
2016-04-19 16:35:47 +0800

    store表

---
+++ database/migrations/2016_04_19_163213_create_store_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStoreTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('store', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->string('name');
+            $table->string('address');
+            $table->string('phone',25);
+            $table->integer('is_distribution');
+            $table->integer('is_display');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('store');
+    }
+}
