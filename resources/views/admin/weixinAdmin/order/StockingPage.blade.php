5dc43bbe060a4b9819ee283e64fdb167d9b162b9
18759758958 <18759758958@163.com>
2016-05-03 09:24:00 +0800

    时间区间搜索

--- resources/views/admin/weixinAdmin/order/StockingPage.blade.php
+++ resources/views/admin/weixinAdmin/order/StockingPage.blade.php
@@ -8,31 +8,54 @@
 @stop
 
 @section('content')
+<div class="f-left right-side-panel">
 
-
-    <div class="f-left right-side-panel">
-
-
-        <div class="breadcrumb-nav">
-            <div class="ui  large breadcrumb">
-                <a class="section">主页</a>
-                <i class="right angle icon divider"></i>
-                <a class="section">订单备货</a>
-            </div>
+    <div class="breadcrumb-nav">
+        <div class="ui  large breadcrumb">
+            <a class="section">主页</a> <i class="right angle icon divider"></i>
+            <a class="section">订单备货</a>
         </div>
+    </div>
 
+    <div class="order-table">
 
-
-        
+        <table class="ui primary striped selectable table " id="orderTable">
+            <thead>
+                <tr>
+                    <th>订单ID</th>
+                    <th>订单号</th>
+                    <th>用户</th>
+                    <th>总额</th>
+                    
+                    <th>下单日期</th>
+                    <th>取单日期</th>
+                    <th>订单详情</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($orders as $order)
+                <tr>
+                    <td>{{$order->id}}</td>
+                    <td>{{$order->order_no}}</td>
+                    <td>{{$order->user_id}}</td>
+                    <td>{{$order->total_amount}}</td>
+                    
+                    <td>{{$order->created_at}}</td>
+                    <td>{{$order->order_dateTime}}</td>
+                    <td>
+                        <a href="{{url('/weixin/admin/order/').'/'.$order->order_no}}" class="ui basic  button ">详情</a>
+                    </td>
+                </tr>
+                @endforeach
+              
+            </tbody>
+        </table>
 
     </div>
 
-
-
+</div>
 @stop
 
 @section('script')
-    <script type="text/javascript">
-        
-    </script>
+<script type="text/javascript"></script>
 @stop
